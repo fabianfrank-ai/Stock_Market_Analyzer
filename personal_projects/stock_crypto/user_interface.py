130a031647f8ed8b9860de2e4b306ac0421a60a2
fabianfrank-ai <frankfabian945@outlook.de>
2025-09-19 21:28:27 +0300

    Merge

--- personal_projects/stock_crypto/user_interface.py
+++ personal_projects/stock_crypto/user_interface.py
@@ -1,3 +1,4 @@
 # Plan is to create the streamlit GUI here for tidiness
 # Don't want to clutter up main.py with UI code
 # Don't really know if I'll actually do this, but it's a nice idea, though time-consuming
+
