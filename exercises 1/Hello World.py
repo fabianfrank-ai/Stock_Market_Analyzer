db9b78251b2cd5e55b437da93f0190e75c564756
fabianfrank-ai <frankfabian945@outlook.de>
2025-11-17 17:22:56 +0200

    Removal of old exercises

--- exercises 1/Hello World.py
+++
@@ -1,4 +0,0 @@
-#define what text you want to display
-text = "Hello World!"
-#print the desired text
-print(text)
--- exercises 1/exercise1_5.py
+++
@@ -1,7 +0,0 @@
-#I hate this.
-#tbh it was just testing how many \t i'd need to get them in line (The worst exercise of both sheets(by a lot))
-print("First Name \t Year of Birth \t Salary\t Tax Percentage")
-print("Matt \t\t 1970 \t\t\t 2000 \t 22.8")
-print("Maria \t\t 1980 \t\t\t 2500 \t 27.7")
-print("Bob \t\t 1990 \t\t\t 1000 \t 19.7")
-
